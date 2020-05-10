0f3a711a92abf4550b68ee5dcebe49ec9f98d459
Maksim Skipin <skipysmaks@gmail.com>
2020-05-10 13:57:13 +0300

    hw complete

---
+++ js-homework/module-04/js/task-02.js
@@ -0,0 +1,35 @@
+const inventory = {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+
+// add new variebles
+const addItem = inventory.add.bind(inventory);
+const removeItem = inventory.remove.bind(inventory);
+//
+
+const invokeInventoryAction = function(itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action(itemName);
+};
+
+invokeInventoryAction('Medkit', addItem);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction('Gas mask', removeItem);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
