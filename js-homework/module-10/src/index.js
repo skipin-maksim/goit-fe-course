c5838210b081fa3c0343e0bffadbed9eee7a1cc1
Maksim Skipin <skipysmaks@gmail.com>
2020-07-13 22:02:10 +0300

    10

--- js-homework/module-10/src/index.js
+++ js-homework/module-10/src/index.js
@@ -1,1 +1,47 @@
 import './styles.css';
+import cardFood from './templates/template-card.hbs';
+import itemsCard from './menu.json';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+  foodList: document.querySelector('.js-menu'),
+  body: document.querySelector('body'),
+  switchInput: document.querySelector('.js-switch-input'),
+};
+
+const markup = cardFood(itemsCard);
+
+refs.foodList.insertAdjacentHTML('beforeend', markup);
+
+refs.switchInput.addEventListener('change', changeThemes);
+
+const activeTheme = localStorage.getItem('theme');
+
+// смена темы
+function add(first, second) {
+  refs.body.classList.add(first);
+  refs.body.classList.remove(second);
+
+  localStorage.setItem('theme', first);
+}
+
+function changeThemes() {
+  refs.switchInput.checked
+    ? add(Theme.DARK, Theme.LIGHT)
+    : add(Theme.LIGHT, Theme.DARK);
+}
+
+// загрузка темы по умолчанию
+function loadTheme() {
+  if (activeTheme === Theme.DARK) {
+    add(Theme.DARK, Theme.LIGHT);
+
+    refs.switchInput.checked = true;
+  }
+}
+
+loadTheme();
