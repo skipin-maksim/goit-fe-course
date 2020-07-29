5cfb3247c3365d58072c8fee8916bae980c670e2
Maksim Skipin <skipysmaks@gmail.com>
2020-07-30 01:03:48 +0300

    del

--- js-homework/module-13.2/src/js/observer.js
+++
@@ -1,9 +0,0 @@
-import { loadMoreData, search } from '../index';
-
-export const iObserver = new IntersectionObserver((entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) loadMoreData(search);
-  });
-});
-
-export default iObserver;
